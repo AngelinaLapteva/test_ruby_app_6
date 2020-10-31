4bfd7950be29cd92c81c4b16153092ad51dafb6a
Angelina <angelinalaptev@gmail.com>
2020-10-30 20:04:57 -0400

    adding more tests

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,7 @@
 class CategoriesController < ApplicationController
 
+  before_action :require_admin, except: [:index, :show]
+
   def new
     @category = Category.new
   end
@@ -28,4 +30,11 @@ class CategoriesController < ApplicationController
     params.require(:category).permit(:name)
   end
 
-end 
+  def require_admin
+    if !(logged_in? && current_user.admin?)
+      flash[:alert] = "Only admins can perfoem this actiona"
+      redirect_to categories_path
+    end
+  end
+
+end
